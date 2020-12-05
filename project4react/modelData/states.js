5d31ceab634dcbb4140783abc5cbdcc34cb66647
DeeG <intern@infinite.com>
2020-12-05 16:07:49 +0800

    lab 4,5 finished

--- project4react/modelData/states.js
+++ project4react/modelData/states.js
@@ -10,62 +10,20 @@
 
 var cs142models;
 
-if (cs142models === undefined) {
+if (!cs142models) {
    cs142models = {};
 }
 
-cs142models.statesModel = function() {
-   return [
-      'Alabama', 
-      'Alaska', 
-      'Arizona', 
-      'Arkansas', 
-      'California',
-      'Kansas', 
-      'Kentucky', 
-      'Louisiana', 
-      'Maine', 
-      'Maryland',
-      'South Dakota', 
-      'Tennessee', 
-      'Texas', 
-      'Utah', 
-      'Vermont',
-      'Montana', 
-      'Nebraska', 
-      'Nevada', 
-      'New Hampshire', 
-      'New Jersey',
-      'Hawaii', 
-      'Idaho', 
-      'Illinois', 
-      'Indiana', 
-      'Iowa',
-      'New Mexico', 
-      'New York', 
-      'North Carolina', 
-      'North Dakota', 
-      'Ohio',
-      'Oklahoma', 
-      'Oregon', 
-      'Pennsylvania', 
-      'Rhode Island', 
-      'South Carolina',
-      'Massachusetts', 
-      'Michigan', 
-      'Minnesota', 
-      'Mississippi', 
-      'Missouri',
-      'Colorado', 
-      'Connecticut', 
-      'Delaware', 
-      'Florida', 
-      'Georgia',
-      'Virginia', 
-      'Washington', 
-      'West Virginia', 
-      'Wisconsin', 
-      'Wyoming'
-   ];
+cs142models.statesModel = function () {
+   return ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+      'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
+      'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+      'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+      'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+      'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+      'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+      'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
+      'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+      'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 };
 
